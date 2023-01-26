855667152dc3a1e53f599d16ecedd04040f01040
msobstel <maciej.sobstel@gmail.com>
2023-01-26 21:52:21 +0100

    added a way to compose a random set of meals with aroud 2500 calories

---
+++ foody/foody.py
@@ -0,0 +1,35 @@
+import requests
+import os
+from dotenv import load_dotenv
+import csv
+import random
+import json
+
+
+with open('epi_r.csv', 'r') as csv_file:
+    csv_reader = csv.reader(csv_file)
+    records = [row for row in csv_reader]
+
+
+menu = []
+
+for record in records:
+    dictionary = {}
+    dictionary[record[0]] = record[2]
+    menu.append(dictionary)
+
+
+meals = []
+cals = []
+calories = 0.0
+while calories < 2500.0:
+    random_meal = random.choice(menu)
+    for key, value in random_meal.items():
+        if value != '':
+            calories += float(value)
+            meals.append(key)
+            cals.append(value)
+
+
+print(meals)
+print(cals)
