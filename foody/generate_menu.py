105d2e5e3535bfe905d5400b4602ae6898a2f1a8
msobstel <maciej.sobstel@gmail.com>
2023-02-27 13:16:45 +0100

    changed conditions function so it is more universal, created a better massage for user

--- foody/generate_menu.py
+++ foody/generate_menu.py
@@ -2,10 +2,9 @@ import argparse
 import random
 from statistics import mean, median
 from typing import Callable, List
-import pdb
 
 from fetch_data import select_from_db
-from conditions import cond_calories, cond_protein, cond_fat, cond_sodium
+from conditions import cond, generate_total_value
 
 
 def fetch_and_filter_meals(meal_type: str, statistics_callable: Callable = median) -> List:
@@ -45,11 +44,33 @@ def generate_menu(min_calories: float, max_calories: float, min_protein: float, 
             meals.append(random.choice(lunch))
         for i in range(number_of_dinners - 1):
             meals.append(random.choice(dinner))
-        if cond_calories(meals, min_calories, max_calories):
-            if cond_protein(meals, min_protein, max_protein):
-                if cond_fat(meals, min_fat, max_fat):
-                    if cond_sodium(meals, min_sodium, max_sodium):
-                        return meals
+        total_calories = generate_total_value(1, meals)
+        total_protein = generate_total_value(2, meals)
+        total_fat = generate_total_value(3, meals)
+        total_sodium = generate_total_value(4, meals)
+        if cond(min_calories, max_calories, total_calories):
+            if cond(min_protein, max_protein, total_protein):
+                if cond(min_fat, max_fat, total_fat):
+                    if cond(min_sodium, max_sodium, total_sodium):
+                        return meals, total_calories, total_protein, total_fat, total_sodium
+
+
+def generate_message(min_calories: float, max_calories: float, min_protein: float, max_protein: float, min_fat: float, max_fat: float, min_sodium: float, max_sodium: float, number_of_breakfasts: int = 1, number_of_lunches: int = 1, number_of_dinners: int = 1):
+    menu, total_calories, total_protein, total_fat, total_sodium = generate_menu(min_calories, max_calories, min_protein, max_protein, min_fat, max_fat, min_sodium, max_sodium, number_of_breakfasts, number_of_lunches, number_of_dinners)
+    meal_names = []
+    for meal in menu:
+        meal_names.append(meal[0])
+    meal_types = []
+    for meal in menu:
+        meal_types.append(meal[5])
+    meals = [f"{a}({b})" for a, b in zip(meal_names, meal_types)]
+    first_line = "Menu: " + ", ".join(map(str, meals)) + '\n'
+    secord_line = "Total calories: " + str(total_calories) + '\n'
+    third_line = "Total protein: " + str(total_protein) + '\n'
+    fourth_line = "Total fat: " + str(total_fat) + '\n'
+    fifth_line = "Total sodium: " + str(total_sodium) + '\n'
+    text = first_line + secord_line + third_line + fourth_line + fifth_line
+    return text
 
 
 # if __name__ == '__main__':
@@ -67,5 +88,6 @@ def generate_menu(min_calories: float, max_calories: float, min_protein: float, 
 #     parser.add_argument('number_of_dinners', type=int, help='Number of dinners')
 #     args = parser.parse_args()
 
-#     print(generate_menu(args.min_calories, args.max_calories, args.min_protein, args.max_protein, args.min_fat, args.max_fat, args.min_sodium, args.max_sodium, args.number_of_breakfasts, args.number_of_lunches, args.number_of_dinners))
-print(generate_menu(1000, 2000, 1, 200, 1, 200, 1, 200, 2, 2, 2))
+#     generate_menu(args.min_calories, args.max_calories, args.min_protein, args.max_protein, args.min_fat, args.max_fat, args.min_sodium, args.max_sodium, args.number_of_breakfasts, args.number_of_lunches, args.number_of_dinners)
+
+print(generate_message(1000, 2500, 1, 200, 1, 200, 1, 200, 2, 1, 2))
